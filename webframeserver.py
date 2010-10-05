d331b4735fb5e09ba7ce620176ce6c4d3c7ce9b5
Toni Alatalo <toni@playsign.net>
2010-10-06 01:37:21 +0300

    params from web - kind of works already, but directions are wrong

--- webframeserver.py
+++ webframeserver.py
@@ -2,36 +2,39 @@ import clr
 clr.AddReference('OpenSim.Framework')
 clr.AddReference('OpenSim.Region.Framework')
 clr.AddReference('System.Drawing')
+clr.AddReference('OpenMetaverseTypes')
 
 from System import Convert
 from System.Collections import Hashtable
 from System.IO import MemoryStream
 from System.Drawing.Imaging import Encoder, EncoderParameters, EncoderParameter, ImageCodecInfo
+
+from OpenMetaverse import Vector3, Quaternion
+
 from OpenSim.Region.Framework.Interfaces import IRegionModule
 from OpenSim.Framework import MainServer
 
+from math import *
 import regionmodule
 
-URL = "/myurl3/"
+URL = "/renderimg/"
 
 class WebFrameserverModule(regionmodule.RegionModule):
     def Initialise(self, scene, configsource):
         self.scene = scene
 
-        print "jee", MainServer
+        print self.render() #to test upon opensim console py-reload without doing anything else
+
         server = MainServer.Instance
         server.AddHTTPHandler(URL, self.onHTTPGetImage)
-
-        print self.render() #to test upon opensim console py-reload without doing anything else
 
     def Close(self):
         print "pois"
         MainServer.Instance.RemoveHTTPHandler("GET", URL)
 
-    def render(self):
+    def render(self, campos=Vector3(80, 127.5, 50), camdir=-Vector3.UnitZ):
         imagemod = self.scene.Modules['Warp3DImageModule']
-        bmp = imagemod.CreateMapTile()
-        print type(bmp)
+        bmp = imagemod.CreateViewImage(campos, camdir, 60, 700, 700)
 
         imgstream = MemoryStream()
         encparams = EncoderParameters()
@@ -42,7 +45,35 @@ class WebFrameserverModule(regionmodule.RegionModule):
     def onHTTPGetImage(self, params):
         print params
 
-        img = self.render()
+        for entry in params:
+            print entry.Key, entry.Value
+
+        camposx = maybefloat(params['camposx']) or 127.5
+        camposy = maybefloat(params['camposy']) or 127.5
+        camposz = maybefloat(params['camposz']) or 60
+        #print camposx, camposy, camposz
+        campos = Vector3(camposx, camposy, camposz)
+
+        #camortx = maybefloat(params['camortx'])
+        #camorty = maybefloat(params['camorty'])
+        #camortz = maybefloat(params['camortz'])
+        #camortw = maybefloat(params['camortw'])
+        #if None in [camortx, camorty, camortz, camortw]:
+        #    camdir = -Vector3.UnitY
+        #else:
+        #    camdir = Vector3.UnitX * Quaternion(camortx, camorty, camortz, camortw)
+        camyaw = maybefloat(params['camyaw'])
+        campitch = maybefloat(params['campitch'])
+        camroll = maybefloat(params['camroll'])
+        if None in [camyaw, campitch, camroll]:
+            camdir = -Vector3.UnitZ
+        else:
+            dx = sin(radians(camyaw)) * cos(radians(campitch))
+            dy = sin(radians(-campitch))
+            dz = cos(radians(camyaw)) * cos(radians(campitch))
+            camdir = Vector3(dx, dy, dz)
+        
+        img = self.render(campos, camdir)
         jpeg = img.ToArray()
 
         return Hashtable({
@@ -56,3 +87,9 @@ def getEncoderInfo(mimetype): #ported to py from the c# example on msdn, which i
     for e in encoders:
         if e.MimeType == mimetype:
             return e
+
+def maybefloat(s):
+    try:
+        return float(s)
+    except: #ValueError, TypeError:
+        return None
