bdc1139d376174c582120a9e23f28eea64c2d83d
Toni Alatalo <toni@playsign.net>
2010-10-05 21:37:06 +0300

    i see an image!

--- webframeserver.py
+++ webframeserver.py
@@ -1,12 +1,18 @@
 import clr
 clr.AddReference('OpenSim.Framework')
 clr.AddReference('OpenSim.Region.Framework')
+clr.AddReference('System.Drawing')
 
+from System import Convert
 from System.Collections import Hashtable
+from System.IO import MemoryStream
+from System.Drawing.Imaging import Encoder, EncoderParameters, EncoderParameter, ImageCodecInfo
 from OpenSim.Region.Framework.Interfaces import IRegionModule
 from OpenSim.Framework import MainServer
 
 import regionmodule
+
+URL = "/myurl3/"
 
 class WebFrameserverModule(regionmodule.RegionModule):
     def Initialise(self, scene, configsource):
@@ -14,21 +20,39 @@ class WebFrameserverModule(regionmodule.RegionModule):
 
         print "jee", MainServer
         server = MainServer.Instance
-        server.AddHTTPHandler("/myurl/", self.onHTTPGetImage)
+        server.AddHTTPHandler(URL, self.onHTTPGetImage)
+
+        print self.render() #to test upon opensim console py-reload without doing anything else
 
     def Close(self):
         print "pois"
-        MainServer.Instance.RemoveHTTPHandler("GET", "/myurl/")
+        MainServer.Instance.RemoveHTTPHandler("GET", URL)
+
+    def render(self):
+        imagemod = self.scene.Modules['Warp3DImageModule']
+        bmp = imagemod.CreateMapTile()
+        print type(bmp)
+
+        imgstream = MemoryStream()
+        encparams = EncoderParameters()
+        encparams.Param[0] = EncoderParameter(Encoder.Quality, 95)
+        bmp.Save(imgstream, getEncoderInfo("image/jpeg"), encparams)
+        return imgstream
 
     def onHTTPGetImage(self, params):
         print params
 
-        imagemod = self.scene.Modules['Warp3DImageModule']
-        img = imagemod.CreateMapTile()
-        print type(img)
+        img = self.render()
+        jpeg = img.ToArray()
 
         return Hashtable({
             "int_response_code": 200,
-            "str_response_string": "terve",
-            "content_type": "text/plain"
+            "str_response_string": Convert.ToBase64String(jpeg),
+            "content_type": "image/jpeg"
             })
+
+def getEncoderInfo(mimetype): #ported to py from the c# example on msdn, which is also in WorldMapModule
+    encoders = ImageCodecInfo.GetImageEncoders()
+    for e in encoders:
+        if e.MimeType == mimetype:
+            return e
